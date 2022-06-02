01307ce36b8a81e1e45306b61ad9f0b3d9300c5c
王健 <1536022504@qq.com>
2022-06-02 11:43:22 +0800

    使用libcurl库下载http图片例子

---
+++ libcurl_test/main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <curl/curl.h>
+/* For older cURL versions you will also need
+#include <curl/types.h>
+#include <curl/easy.h>
+*/
+#include <string>
+
+size_t write_data(void* ptr, size_t size, size_t nmemb, FILE* stream) {
+	size_t written = fwrite(ptr, size, nmemb, stream);
+	return written;
+}
+
+int main(void) {
+	CURL* curl;
+	FILE* fp;
+	CURLcode res;
+	char* url = (char*)"http://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png";
+	char outfilename[FILENAME_MAX] = "d:\\111.png";
+	curl = curl_easy_init();
+	if (curl) {
+		fp = fopen(outfilename, "wb");
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+		res = curl_easy_perform(curl);
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+		fclose(fp);
+	}
+	return 0;
+}
